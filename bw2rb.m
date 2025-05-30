5aa3e45d1e132e8acedcbf3ff6c3696c58921224
marwyp <marekwypich@gmail.com>
2025-05-30 22:43:45 +0200

    fix bw2rb

--- bw2rb.m
+++ bw2rb.m
@@ -9,16 +9,20 @@ function [rb] = bw2rb(bw, scs)
         end
         rb = [25 52 79 106 133 160 188 216 242 270];
     elseif scs == 30
-        if ~ismember(bw, 5 : 5 : 100)
+        if ~ismember(bw, [5 : 5 : 50, 60 : 10 : 100])
             error("Wrong BW configuration!")
         end
         rb = [11 24 38 51 65 78 92 106 119 133 162 189 217 245 273];
     elseif scs == 60
-        if ~ismember(bw, 10 : 5 : 100)
+        if ~ismember(bw, [10 : 5 : 50, 60 : 10 : 100])
             error("Wrong BW configuration!")
         end
         rb = [-1 11 18 24 31 38 44 51 58 65 79 93 107 121 135];
     end
-    rb = rb(bw / 5);
+    if bw <= 50
+        rb = rb(bw / 5);
+    else
+        rb = rb(10 + (bw - 50) / 10);
+    end
 end
 
